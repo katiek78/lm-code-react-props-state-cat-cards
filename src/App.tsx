8c613019261fcc4a1db41b8c5483c39795997a39
katiek78 <kermodekatie@gmail.com>
2023-03-11 20:51:01 +0000

    Complete Activity 7

--- src/App.tsx
+++ src/App.tsx
@@ -81,15 +81,23 @@ function App(): JSX.Element {
 				 species: 'Tiny Cat',
 				 favFoods: ['milk'],
 				 birthYear: 2021,
-			 }
+			 },
+			 {
+				name: "Captain Catface",
+				species: "Sea Cattain",
+				favFoods: ["fish, rum"],
+				birthYear: 2016,
+			  }
 	 ]);
 
 	 console.log("Our pretties 😻: ", cats)
 
+	 const catCount = cats.length;
+
 	return (
 		<>
 			<Navbar />
-			<Header />
+			<Header count={catCount} />
 
 			<main>
 				<div className='cards__wrapper'>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,15 @@
-const Header: React.FC = () => (
+const Header: React.FC<HeaderProps> = ({count}) => (
 	<header className='header__container'>
 		<h1 className='header__title header__welcome'>Welcome to React!</h1>
 		<h2 className='header__title'>
-			There are currently 0 Cats in this Cat App
+			There are currently {count} Cats in this Cat App
 		</h2>
 	</header>
 );
+
+
+interface HeaderProps{
+    count: number;    
+}
 
 export default Header;
